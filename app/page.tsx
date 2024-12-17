c967e27710889802a9483e3084806738f5d07d3f
reggidev <reggidev@gmail.com>
2024-12-17 14:19:31 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from './_components/ui/button'
+
 const HomePage = () => {
-  return <h1>Hello World!</h1>
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Button>Teste</Button>
+    </>
+  )
 }
 
 export default HomePage
