76674fad4099c56ed4de3831b9cd62ab5e618075
reggidev <reggidev@gmail.com>
2025-01-10 14:01:55 -0300

    refactor: add new date

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -11,8 +11,8 @@ export const getDashboard = async (month: string) => {
   /* Total Invested Card */
   const where = {
     date: {
-      gte: new Date(`2024-${month}-01`),
-      lt: new Date(`2024-${month}-31`),
+      gte: new Date(`2025-${month}-01`),
+      lt: new Date(`2025-${month}-31`),
     },
   }
 
